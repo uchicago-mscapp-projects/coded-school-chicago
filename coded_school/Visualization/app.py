b0034cbd46c1700a8d5975783ca240719110c82d
apichat-klang <apichat@uchicago.edu>
2024-02-18 08:27:32 -0600

    add app.py

--- coded_school/Visualization/app.py
+++ coded_school/Visualization/app.py
@@ -1,9 +1,11 @@
-from dash import Dash, html
+from dash import Dash, html, doc
+import plotly.express as px
 
 app = Dash(__name__)
 
 app.layout = html.Div([
-    html.Div(children='Hello World')
+    html.Div(children="Chicago's Median Income per zip code"),
+    #doc.Graph(figure=px.scatter(df, x="sepal_width", y="sepal_length"))
 ])
 
 if __name__ == '__main__':
