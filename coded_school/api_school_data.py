e102e93a5de0677bebe0d71eabc77361c1b546a4
Amber Avila <amberavila@Ambers-MacBook-Air.local>
2024-02-29 14:47:29 -0600

    Finalized Data

--- coded_school/api_school_data.py
+++ coded_school/api_school_data.py
@@ -21,7 +21,7 @@ filtered_df = hs_df[['long_name', 'zip', 'school_type', 'primary_category',
                     'mobility_rate_pct', 'chronic_truancy_pct', 'sat_grade_11_score_school',
                     'one_year_dropout_rate_year','one_year_dropout_rate_year_1', 
                     'suspensions_per_100_students_1','suspensions_per_100_students_2']]
-    #could not find 'school_survery_parent_response_rate_pct'
+    #could not find 'school _survery_parent_response_rate_pct'
 
 #Cleaned Data
 def keep():
@@ -37,8 +37,6 @@ def keep():
                     'mobility_rate_pct', 'chronic_truancy_pct', 'sat_grade_11_score_school',
                     'one_year_dropout_rate_year','one_year_dropout_rate_year_1', 
                     'suspensions_per_100_students_1','suspensions_per_100_students_2']]
-
-    #loop find unique value -- 1 2 3 
 
     growth_mapping = {'Far Below Average': 1, 'Below Average': 2, 'Average': 3, 'Above Average': 4, 'Far Above Average': 5}
     attainment_mapping = {'Far Below Expectations': 1, 'Below Average': 2, 'Average': 3, 'Above Average': 4, 'Met Expectations': 5, 'Far Above Expectations': 6}
@@ -62,18 +60,16 @@ def clean_columns():
     
     filtered_df = df[df['primary_category'] == 'HS']
     
-    filtered_df = filtered_df[['zip', 'student_growth_rating', 'student_attainment_rating', 
+    filtered_df = filtered_df[['zip', 'student_attainment_rating', 
                     'culture_climate_rating', 'mobility_rate_pct', 'chronic_truancy_pct', 
                     'sat_grade_11_score_school','one_year_dropout_rate_year','one_year_dropout_rate_year_1', 
                     'suspensions_per_100_students_1','suspensions_per_100_students_2']]
 
-    growth_mapping = {'Far Below Average': 1, 'Below Average': 2, 'Average': 3, 'Above Average': 4, 'Far Above Average': 5}
-    attainment_mapping = {'Far Below Expectations': 1, 'Below Average': 2, 'Average': 3, 'Above Average': 4, 'Met Expectations': 5, 'Far Above Expectations': 6}
-    climate_mapping = {'Not Enough Data': pd.NA, 'Well Organized': 5, 'Organized': 4, 'Moderately Organized': 3, 'Partially Organized': 2, 'Not Yet Organized': 1}
+    attainment_mapping = {'FAR BELOW EXPECTATIONS': 1, 'BELOW AVERAGE': 2, 'AVERAGE': 3, 'ABOVE AVERAGE': 4, 'MET EXPECTATIONS': 5, 'FAR ABOVE EXPECTATIONS': 6}
+    climate_mapping = {'NOT ENOUGH DATA': None, 'WELL ORGANIZED': 5, 'ORGANIZED': 4, 'MODERATELY ORGANIZED': 3, 'PARTIALLY ORGANIZED': 2, 'NOT YET ORGANIZED': 1}
 
-    filtered_df['student_growth_rating'] = filtered_df['student_growth_rating'].replace(growth_mapping)
-    filtered_df['student_attainment_rating'] = filtered_df['student_attainment_rating'].replace(attainment_mapping)
-    filtered_df['culture_climate_rating'] = filtered_df['culture_climate_rating'].replace(climate_mapping)
+    filtered_df['student_attainment_rating'] = filtered_df['student_attainment_rating'].map(attainment_mapping)
+    filtered_df['culture_climate_rating'] = filtered_df['culture_climate_rating'].map(climate_mapping)
 
     filtered_df['drop_out_rate'] = (filtered_df['one_year_dropout_rate_year'] + filtered_df['one_year_dropout_rate_year_1']) / 2
     filtered_df['suspensions_rate'] = (filtered_df['suspensions_per_100_students_1'] + filtered_df['suspensions_per_100_students_2']) / 2
@@ -82,22 +78,25 @@ def clean_columns():
 
     return filtered_df
 
-def average_by_zip():
-    data = clean_columns()
+def average_by_zip(df):
+    df.fillna(df.mean(), inplace = True)
 
-    unique_zips = numpy.unique[data['zip']]
+    avg_df = df.groupby('zip').mean().reset_index()
+
+    return avg_df
+
     
-#Get url to CPS school page
+#Get url to CPS school page --- do we need this? had it there before and i didnt take it out 
 for index, row in hs_df.iterrows():
     hs_df.at[index, "cps_school_profile"] = row["cps_school_profile"]["url"]
 
 
 def cleaned_data():
-        data = clean()
+        data = average_by_zip(clean_columns())
         income = create_dataframe()
 
-        frames = [data, income]
+        data['zip'] = data['zip'].astype(str)
 
-        merged_data = pd.concat(frames)
+        merged_data = pd.merge(data, income, on = 'zip', how = 'left')
 
         return merged_data 
