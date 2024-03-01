bb26c5f4e154c201483e108ec160d36464f74e95
Charisma Lambert <charismalambert@Charismas-MacBook-Pro.local>
2024-03-01 12:19:05 -0600

    updates

--- coded_school/analysis.py
+++ coded_school/analysis.py
@@ -6,24 +6,12 @@ import pandas as pd
 data = pd.read_csv('merged_data.csv')
 data = data.dropna()
 
-#x = data['student_attainment_rating'].values.reshape(1, -1)
-#y1 = data['med_income'].values
-#y2 = data['poverty_rate'].values
-#y3 = data['unemp_rate'].values
-#y4 = data['hs_enrol_rate'].values
-
-
-#model = LinearRegression()
-#model.fit(x, y1)
-#print(model.intercept_)
-#print(model.coef_)
-
 # Linear Regression: Median Income on Avg SAT score
 x = data['sat_grade_11_score_school'].values.reshape(-1, 1)
 y = data['med_income'].values
 model = LinearRegression().fit(x, y)
-print(model.coef_)
-print(model.intercept_)
+print("med_income_intercept:", model.intercept_)
+print("sat_coef:", model.coef_)
 prediction = model.predict(x)
 
 # Scatter plot of Avg SAT Score and Median Income 
@@ -32,3 +20,17 @@ plt.plot(data['sat_grade_11_score_school'], prediction, color = "red")
 plt.xlabel("Average 11th Grade SAT Score of Schools in Zip Code")
 plt.ylabel("Median Househould Income in this Zip Code")
 plt.show()
+
+#Multiple Regression
+x1 = data[['student_attainment_rating', 'culture_climate_rating', 
+          'mobility_rate_pct', 'chronic_truancy_pct', 'sat_grade_11_score_school',
+          'drop_out_rate', 'suspensions_rate']].values
+y1 = data['med_income'].values
+model1 = LinearRegression().fit(x1, y1)
+school_attributes = ['student_attainment_rating', 'culture_climate_rating', 
+          'mobility_rate_pct', 'chronic_truancy_pct', 'sat_grade_11_score_school',
+          'drop_out_rate', 'suspensions_rate']
+
+for category, coef in zip(school_attributes, model1.coef_):
+    print(category, ":", coef)
+print("med_income_intercept:", model1.intercept_)
